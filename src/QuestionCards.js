6174421790a935493a8110db050954a243cbea68
Patrick Morrison <95713259+patgramo@users.noreply.github.com>
2022-11-10 11:17:19 -0800

    Completed the personality question The first part of the quiz maps through an array of objects to display the questions and answers and collects the personality data in a state array.

--- src/QuestionCards.js
+++ src/QuestionCards.js
@@ -4,20 +4,80 @@ import questionsArray from "../data/personalityQuestions";
 export default function QuestionCards() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState();
-  const [score, setScore] = useState([]);
+  const [score, setScore] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
   useEffect(() => {
     console.log(score);
   }, [score]);
-
   const handleAnswerOptionClick = (personality) => {
-    setScore(score.concat(personality));
-    console.log(score);
+    if (personality === "lazy") {
+      let lazyScore = [1, 0, 0, 0, 0, 0, 0, 0];
+      let newScore = score.map((a, i) => a + lazyScore[i]);
+      setScore(newScore);
+    } else if (personality === "jock") {
+      let jockScore = [0, 1, 0, 0, 0, 0, 0, 0];
+      let newScore = score.map((a, i) => a + jockScore[i]);
+      setScore(newScore);
+    } else if (personality === "cranky") {
+      let crankyScore = [0, 0, 1, 0, 0, 0, 0, 0];
+      let newScore = score.map((a, i) => a + crankyScore[i]);
+      setScore(newScore);
+    } else if (personality === "smug") {
+      let smugScore = [0, 0, 0, 1, 0, 0, 0, 0];
+      let newScore = score.map((a, i) => a + smugScore[i]);
+      setScore(newScore);
+    } else if (personality === "normal") {
+      let normalScore = [0, 0, 0, 0, 1, 0, 0, 0];
+      let newScore = score.map((a, i) => a + normalScore[i]);
+      setScore(newScore);
+    } else if (personality === "peppy") {
+      let peppyScore = [0, 0, 0, 0, 0, 1, 0, 0];
+      let newScore = score.map((a, i) => a + peppyScore[i]);
+      setScore(newScore);
+    } else if (personality === "snooty") {
+      let snootyScore = [0, 0, 0, 0, 0, 0, 1, 0];
+      let newScore = score.map((a, i) => a + snootyScore[i]);
+      setScore(newScore);
+    } else if (personality === "sisterly") {
+      let sisterlyScore = [0, 0, 0, 0, 0, 0, 0, 1];
+      let newScore = score.map((a, i) => a + sisterlyScore[i]);
+      setScore(newScore);
+    }
     //I need to fix the setScore max method. What if there are 2 indices that have the max number?
     const nextQuestion = currentQuestion + 1;
-
+    const max = Math.max(...score);
+    const index = score.indexOf(max);
     if (nextQuestion < questionsArray.length) {
       setCurrentQuestion(nextQuestion);
-    } else {
+    } else if (nextQuestion >= questionsArray.length && index === 0) {
+      setScore("Lazy");
+      document.getElementById("results").classList.remove("hide");
+      document.getElementById("question-container").classList.add("blur");
+    } else if (nextQuestion >= questionsArray.length && index === 1) {
+      setScore("Jock");
+      document.getElementById("results").classList.remove("hide");
+      document.getElementById("question-container").classList.add("blur");
+    } else if (nextQuestion >= questionsArray.length && index === 2) {
+      setScore("Cranky");
+      document.getElementById("results").classList.remove("hide");
+      document.getElementById("question-container").classList.add("blur");
+    } else if (nextQuestion >= questionsArray.length && index === 3) {
+      setScore("Smug");
+      document.getElementById("results").classList.remove("hide");
+      document.getElementById("question-container").classList.add("blur");
+    } else if (nextQuestion >= questionsArray.length && index === 4) {
+      setScore("Normal");
+      document.getElementById("results").classList.remove("hide");
+      document.getElementById("question-container").classList.add("blur");
+    } else if (nextQuestion >= questionsArray.length && index === 5) {
+      setScore("Peppy");
+      document.getElementById("results").classList.remove("hide");
+      document.getElementById("question-container").classList.add("blur");
+    } else if (nextQuestion >= questionsArray.length && index === 6) {
+      setScore("Snooty");
+      document.getElementById("results").classList.remove("hide");
+      document.getElementById("question-container").classList.add("blur");
+    } else if (nextQuestion >= questionsArray.length && index === 7) {
+      setScore("Sisterly");
       document.getElementById("results").classList.remove("hide");
       document.getElementById("question-container").classList.add("blur");
     }
